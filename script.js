5bf844b019318ad6cf54a124a9cc908182024e42
reputation0809 <50741961+reputation0809@users.noreply.github.com>
2025-01-24 08:45:48 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,333 @@
+document.getElementById("enterButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("choose_page").style.display = "block";
+});
+
+document.getElementById("backButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("homepage").style.display = "block";
+  document.getElementById("choose_page").style.display = "none";
+});
+
+document.getElementById("backButton_page3").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("choose_page").style.display = "block";
+  document.getElementById("govern_page").style.display = "none";
+});
+
+document.getElementById("backButton_page3_1").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("govern_page").style.display = "block";
+  document.getElementById("care_page").style.display = "none";
+});
+
+document.getElementById("governButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("govern_page").style.display = "block";
+  document.getElementById("choose_page").style.display = "none";
+});
+
+document.getElementById("moneyButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("money_page").style.display = "block";
+  document.getElementById("choose_page").style.display = "none";
+});
+
+document.getElementById("communityButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("community_page").style.display = "block";
+  document.getElementById("choose_page").style.display = "none";
+});
+
+document.getElementById("careButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("care_page").style.display = "block";
+  document.getElementById("govern_page").style.display = "none";
+});
+
+document.getElementById("taitungButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("county_page").style.display = "block";
+  document.getElementById("govern_page").style.display = "none";
+});
+
+document.getElementById("backButton_page3_3").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("govern_page").style.display = "block";
+  document.getElementById("county_page").style.display = "none";
+});
+
+document.getElementById("backButton_page4").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("choose_page").style.display = "block";
+  document.getElementById("money_page").style.display = "none";
+});
+
+document.getElementById("backButton_page5").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("choose_page").style.display = "block";
+  document.getElementById("community_page").style.display = "none";
+});
+
+document.getElementById("affairButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("affair_homepage").style.display = "block";
+  document.getElementById("govern_page").style.display = "none";
+});
+
+document.getElementById("backButton_affair_homepage").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("govern_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("honor_identity").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("honor_identity_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_honor_identity_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("honor_identity_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("home").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("home_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_home_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("home_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("study").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("study_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_study_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("study_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("job").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("job_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_job_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("job_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("train").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("train_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_train_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("train_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("medical").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("medical_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_medical_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("medical_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("care").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("care_page2").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_care_page2").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("care_page2").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("retire_money").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("retire_money_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_retire_money_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("retire_money_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("identity").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("identity_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_identity_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("identity_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("funeral").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("funeral_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_funeral_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("funeral_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("crucial").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("crucial_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_crucial_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("crucial_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("imergency").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("imergency_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_imergency_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("imergency_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("season").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("season_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_season_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("season_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("funeral_care").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("funeral_care_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_funeral_care_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("funeral_care_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("school").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("school_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_school_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("school_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("lunch").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("lunch_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_lunch_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("lunch_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("dependent_foundation").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("dependent_foundation_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_dependent_foundation_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("dependent_foundation_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("medical_care").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("medical_care_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_medical_care_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("medical_care_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("dependent_work_train").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("dependent_work_train_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_dependent_work_train_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("dependent_work_train_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+document.getElementById("dependent_other").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("dependent_other_page").style.display = "block";
+  document.getElementById("affair_homepage").style.display = "none";
+});
+
+document.getElementById("backButton_dependent_other_page").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("dependent_other_page").style.display = "none";
+  document.getElementById("affair_homepage").style.display = "block";
+});
+
+function downloadImage(url, filename){
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
